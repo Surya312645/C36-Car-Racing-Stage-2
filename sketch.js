9690df152112ed0c8eef134ee335a405033c34f8
Surya312645 <66342517+Surya312645@users.noreply.github.com>
2020-09-14 21:50:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database, playerCount = 0, gameState = 0;
+var form, player, game;
+var allPlayers;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+    game = new Game()
+    game.getState() 
+    game.start()
+
+}
+
+function draw(){
+background("white")  
+if (playerCount == 4){
+    game.update(1)
+}
+if (gameState == 1){
+    game.play()
+} 
+}
